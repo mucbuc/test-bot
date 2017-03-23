70dbde09340bd94f41080674cde1b055324499c5
mbusenitz <mbusenitz@gmail.com>
2017-03-23 07:09:15 -0700

    move git test

---
+++ test/git.js
@@ -0,0 +1,42 @@
+
+const test = require( 'tape' )
+  , GIT = require( '../app/git' )
+  , Session = require( '../app/session' )
+  , path = require( 'path' )
+  , fs = require( 'fs' );
+
+test( 'should fail to checkout', (t) => {
+
+  const logic = new Session();
+
+  GIT
+  .pullRepo( path.join( __dirname, '../' ), 'nonsense' )
+  .then( t.fail.bind( t ) )
+  .catch( error => {
+    t.notEqual( error.indexOf( "did not match any file" ) );
+    t.end();
+  });
+});
+
+test( 'should clone local repo', (t) => {
+
+  const logic = new Session();
+
+  GIT
+  .pullRepo( path.join( __dirname, '../' ) )
+  .then( repo => {
+
+    const testjs = path.join( repo.path, 'test', 'test.js' );
+    fs.stat( testjs, (err, stats) => {
+      
+      if (err) 
+      {
+        console.log( err );
+      }
+
+      t.true( !err && stats.isFile(), "expected file at " + testjs );
+      t.end();
+    });
+  })
+  .catch( t.fail.bind( t ) );
+});
